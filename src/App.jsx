cd93405ff2af811e9de5834499ad80b5e2562d17
ParasP41 <parasvishwakarma198@gmail.com>
2025-01-24 12:02:45 +0530

    Zentry

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from './Components/About'
 import Navbar from './Components/Navbar'
 import Features from './Components/Features'
 import Story from './Components/Story'
+import Contact from './Components/Contact'
+import Footer from './Components/Footer'
 const App = () => {
   return (
     <main className='relative  min-h-screen w-screen overflow-x-hidden'>
@@ -12,6 +14,8 @@ const App = () => {
     <About/>
     <Features/>
     <Story/>
+    <Contact/>
+    <Footer/>
     </main>
   )
 }
